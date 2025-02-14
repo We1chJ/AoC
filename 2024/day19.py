db3867d649452525c3934d15781aa2e02a36e89f
We1chJ <weq511762338@gmail.com>
2025-02-14 17:15:25 -0500

    Revert "workign day18"
    This reverts commit 97cd957a3cc1dfb952995b195bbb41146c1d39ea.

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,11 +1,40 @@
+# def valid(pattern, towels):
+#     if len(pattern) == 0:
+#         return True
+#     for a in towels:
+#         if pattern.startswith(a):
+#             if valid(pattern[len(a):], towels):
+#                 return True
+#     return False
+
+# def main():
+#     with open("input.txt", "r") as file:
+#         lines = file.readlines()
+#         towels = [ a.strip() for a in lines[0].split(", ")]
+#         cnt = 0
+#         for line in lines[2:]:
+#             if valid(line.strip(), towels):
+#                 cnt += 1
+#         print(cnt)
+
+cache = {}
+
 def valid(pattern, towels):
+    if pattern in cache:
+        return cache[pattern]
     if len(pattern) == 0:
-        return True
+        return 1
+    cnt = 0
     for a in towels:
         if pattern.startswith(a):
-            if valid(pattern[len(a):], towels):
-                return True
-    return False
+            if pattern in cache:
+                res = cache[pattern]
+                cnt += res
+            else:
+                res = valid(pattern[len(a):], towels)
+                cnt += res
+    cache[pattern] = cnt
+    return cnt
 
 def main():
     with open("input.txt", "r") as file:
@@ -13,8 +42,8 @@ def main():
         towels = [ a.strip() for a in lines[0].split(", ")]
         cnt = 0
         for line in lines[2:]:
-            if valid(line.strip(), towels):
-                cnt += 1
+            cnt += valid(line.strip(), towels)
         print(cnt)
+
 if __name__ == "__main__":
     main()
