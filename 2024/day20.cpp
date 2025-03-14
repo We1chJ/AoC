8b05be4f3683c724d2744a7c265ba243a89c99af
Welch <weq511762338@gmail.com>
2025-03-14 10:45:29 -0400

    temp save for day 20, debugging rn

---
+++ 2024/day20.cpp
@@ -0,0 +1,92 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    array<int, 2> st = {0, 0};
+    array<int, 2> en = {0, 0};
+    vector<string> map;
+    string line;
+    while(cin >> line){
+        map.push_back(line);
+    }
+    int n = map.size();
+    int m = map[0].size();
+    for(int i =0 ; i <n ;i++){
+        for(int j =0 ; j < m; j++){
+            if(map[i][j] == 'S'){
+                st = {i, j};
+            }
+            if(map[i][j] == 'E'){
+                en= {i, j};
+            }
+        }
+    }
+
+    vector<vector<int>> timeToEnd (n, vector<int>(m, -1));
+    int depth = 0;
+    queue<array<int, 2>> q;
+    q.push(en);
+    while(q.size()){
+        auto [x, y] = q.front();
+        q.pop();
+        if (timeToEnd[x][y] != -1) continue;
+        timeToEnd[x][y] = depth;
+        for (auto [dx, dy] : vector<array<int, 2>>{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}) {
+            int nx = x + dx, ny = y + dy;
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] != '#') {
+                q.push({nx, ny});
+            }
+        }
+        depth++;
+    }
+
+    int timeNormal = timeToEnd[st[0]][st[1]];
+    cout << "timenormal: " << timeNormal << endl;
+    
+    q.push(st);
+    depth = 0;
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
+    vector<vector<set<pair<int, int>>>> ans(n, vector<set<pair<int, int>>>(m));
+    while(q.size()){
+        auto [x, y] = q.front();
+        q.pop();
+        visited[x][y] = true;
+
+        for (auto [dx, dy] : vector<array<int, 2>>{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}) {
+            int nx = x + dx, ny = y + dy;
+            for (auto [ddx, ddy] : vector<array<int, 2>>{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}) {
+                int nnx = nx + ddx, nny = ny + ddy;
+                for (auto [newx, newy] : vector<array<int, 2>>{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}) {
+                    int lastx = nnx + newx, lasty = nny + newy;
+                    if (lastx >= 0 && lastx < n && lasty >= 0 && lasty < m && map[lastx][lasty] != '#') {
+                        if(timeNormal - (depth + timeToEnd[lastx][lasty] + 3) >= 1){
+                            ans[nx][ny].insert({nnx, nny});
+                        }
+                    }
+                }
+            }
+        }
+        for (auto [dx, dy] : vector<array<int, 2>>{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}) {
+            int nx = x + dx, ny = y + dy;
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] != '#') {
+                if(!visited[nx][ny]){
+                    q.push({nx, ny});
+                }
+            }
+        }
+        depth++;
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cnt += ans[i][j].size();
+            for (const auto& p : ans[i][j]) {
+                cout << "(" << i << ", " << j << ") -> (" << p.first << ", " << p.second << ")" << endl;
+            }
+        }
+    }
+    cout << cnt << endl;
+}
